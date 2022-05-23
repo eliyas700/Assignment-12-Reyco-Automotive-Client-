045f6c5d1d186e807567a44694fadc92d24c092d
Eliyas Hossain <eliyashossain700@gmail.com>
2022-05-24 02:12:10 +0600

    Display My orders from DB

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = () => {
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content ">
         {/* <!-- Page content here --> */}
-        <h2 className="text-3xl font-semibold text-purple-400">
+        <h2 className="text-3xl font-semibold text-primary text-center my-3">
           Welcome to Your Dashboard
         </h2>
         <Outlet></Outlet>
@@ -20,6 +20,7 @@ const Dashboard = () => {
         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
         <ul className="menu p-4 overflow-y-auto w-48 lg:w-60 bg-base-100 text-base-content">
           {/* <!-- Sidebar content here --> */}
+          <li className="text-xl font-semibold text-secondary">User Panel</li>
           <li>
             <Link to="/dashboard">My Orders</Link>
           </li>
--- src/Pages/Dashboard/UsersSection/MyOrders.js
+++ src/Pages/Dashboard/UsersSection/MyOrders.js
@@ -1,9 +1,104 @@
-import React from "react";
+import { signOut } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const MyOrders = () => {
+  const [orders, setOrders] = useState([]);
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/orders?userEmail=${user.email}`, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+        .then((res) => {
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem("accessToken");
+            //Home
+            navigate("/");
+          }
+          return res.json();
+        })
+        .then((data) => setOrders(data));
+    }
+  }, [user]);
   return (
     <div>
-      <h3>This is from My Orders</h3>
+      <div>
+        <h4 className="text-xl my-2 font-semibold">
+          Total Orders:{orders?.length}
+        </h4>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Photo</th>
+                <th>Name</th>
+
+                <th>
+                  Price/<small>Piece</small>
+                </th>
+                <th>Quantity</th>
+                <th>Total Price</th>
+                <th className="text-center">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((a, index) => (
+                <tr key={a._id}>
+                  <th>{index + 1}</th>
+                  <th>
+                    <div class="avatar">
+                      <div class="w-8 rounded">
+                        <img src={a.img} alt={a.name} />
+                      </div>
+                    </div>
+                  </th>
+                  <td>{a.name}</td>
+                  <td>${a.price}</td>
+                  <td>{a.quantity}</td>
+                  <td>${a.quantity * a.price}</td>
+                  <td>
+                    {a.payment === "unpaid" && (
+                      <div className="flex justify-between">
+                        <Link
+                          to={`/dashboard/payment/${a._id}`}
+                          className="btn btn-xs btn-success"
+                        >
+                          Pay Now
+                        </Link>
+                        <button className="btn btn-xs btn-error">Cancel</button>
+                      </div>
+                    )}
+                    {/* {a.price && a.paid && (
+                      <div>
+                        <p>
+                          <span className="text-success">Paid</span>
+                        </p>
+                        <p>
+                          <small>
+                            Trans id:
+                            <span className="text-success">
+                              {a.transactionId}
+                            </span>
+                          </small>
+                        </p>
+                      </div>
+                    )} */}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
